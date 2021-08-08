b093fe998474bd3ba9df48c97e8f84e7667116bb
Colin Peters <colinthedev@gmail.com>
2021-08-08 14:26:56 -0600

    React State

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React, { useState } from 'react';
+
 
 // Using map
 const numbers = [1, 4, 9, 16];
@@ -51,14 +53,20 @@ function Greeting(props) {
 
 // Search bar
 const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = (event) => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   );
 };
