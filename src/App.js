1d8f2aaf4e6916237b212f1bf1cdbd893a0d209c
Colin Peters <colinthedev@gmail.com>
2021-08-08 17:22:31 -0600

    Callback Handlers, Lifting State, Controlled Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
 
-
 // Using map
 const numbers = [1, 4, 9, 16];
 
@@ -31,15 +30,41 @@ const App = () => {
     },
   ];
 
+  // Set state on searchTerm, setSearchTerm
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  // Get the value of search input
+  const handleSearch = (event) => setSearchTerm(event.target.value);
+
+  // Check if user input matches stories array
+  // toLowerCase() both values
+  const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Render
   return (
     <div className="App">
       <Greeting name="Colin" age="28" occupation="Front-end developer" />
-      <Search />
+      <Search search={searchTerm} onSearch={handleSearch} />
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 }
+
+const words = [
+  'spray',
+  'limit',
+  'elite',
+  'exuberant',
+  'destruction',
+  'present'
+];
+
+const filteredWords = words.filter(function (word) {
+  return word.length > 6;
+});
+console.log(filteredWords);
 
 // Greeting
 function Greeting(props) {
@@ -52,26 +77,19 @@ function Greeting(props) {
 }
 
 // Search bar
-const Search = () => {
-  const [searchTerm, setSearchTerm] = React.useState('');
+const Search = (props) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={props.search}
+      onChange={props.onSearch}
+    />
+  </div>
+);
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
-    </div>
-  );
-};
-
-// List items
+// Return new stories array, assign ID
 const List = (props) => (
   <ul>
     {props.list.map((item) => (
@@ -80,6 +98,7 @@ const List = (props) => (
   </ul>
 );
 
+// Work with stories array
 const Item = (props) => (
   <li>
     <span>
